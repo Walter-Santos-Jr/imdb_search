ef251b8affb68b92d3086ae3d71edcefacafa525
Walter Jr <wj@tomcix.com>
2020-11-04 14:39:32 -0300

    Searching one model (scope) with options tsearch

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,5 +3,8 @@ class Movie < ApplicationRecord
   include PgSearch::Model
 
   pg_search_scope :search_by_title_and_syllabus,
-      against: [:title, :syllabus]
+      against: [:title, :syllabus],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
