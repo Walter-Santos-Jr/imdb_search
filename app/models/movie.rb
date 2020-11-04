055c4a7dde5559a65b3e51aea9a3fb17378dceb6
Walter Jr <wj@tomcix.com>
2020-11-04 15:45:21 -0300

    Multi-search - Usage

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,9 +5,11 @@ class Movie < ApplicationRecord
   pg_search_scope :search_by_title_and_syllabus,
       against: [:title, :syllabus],
       associated_against: {
-            director: [ :first_name, :last_name ]
+            director: [:first_name, :last_name]
           },
     using: {
       tsearch: { prefix: true } # <-- now `nolan` will return something!
     }
+
+    multisearchable against: [:title, :syllabus]
 end
--- app/models/tv_show.rb
+++ app/models/tv_show.rb
@@ -1,2 +1,14 @@
 class TvShow < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search_by_title_and_syllabus,
+      against: [:title, :syllabus],
+      associated_against: {
+            director: [:first_name, :last_name]
+          },
+    using: {
+      tsearch: { prefix: true } # <-- now `nolan` will return something!
+    }
+
+    multisearchable against: [:title, :syllabus]
 end
